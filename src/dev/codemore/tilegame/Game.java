9e42d2bb5dd31f6843c3ff99e0d3912dec3dd098
Nina Ruiz <nina.ruizandres@gmail.com>
2020-08-31 12:47:28 +0200

    Add thread into game class and create game loop

--- src/dev/codemore/tilegame/Game.java
+++ src/dev/codemore/tilegame/Game.java
@@ -2,11 +2,15 @@ package dev.codemore.tilegame;
 
 import dev.codemore.tilegame.display.Display;
 
-public class Game {
+import javax.swing.plaf.TableHeaderUI;
+
+public class Game implements Runnable {
 
     private Display display;
-
     public int width, height;
+
+    private Boolean running = false;
+    private Thread thread;
 
     // Game constructor
     public Game(String title, int width, int height){
@@ -16,6 +20,52 @@ public class Game {
 
         display = new Display(title, width, height);
 
+    }
+
+    private void init(){
+
+    }
+
+    private void tick(){
+
+    }
+
+    private void render(){
+
+    }
+
+    public void run(){
+
+        init();
+
+        while(running){
+            tick();
+            render();
+        }
+
+        stop();
+
+    }
+
+    public synchronized void start(){
+        if(running){
+            return;
+        }
+        running = true;
+        thread = new Thread(this);
+        thread.start();
+    }
+
+    public synchronized void stop(){
+        if(!running){
+            return;
+        }
+        running = false;
+        try {
+            thread.join();
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
     }
 
 }
