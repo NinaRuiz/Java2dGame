f14fde955fcce94524cbb3c5e969fb3b03109a34
Nina Ruiz <nina.ruizandres@gmail.com>
2020-08-31 12:31:03 +0200

    Create main method and create a new display object.

--- src/dev/codemore/tilegame/Launcher.java
+++ src/dev/codemore/tilegame/Launcher.java
@@ -1,4 +1,15 @@
 package dev.codemore.tilegame;
 
+import dev.codemore.tilegame.display.Display;
+
 public class Launcher {
+
+    public static void main(String[] args){
+
+        // Create new Display object
+        new Display("Title", 300, 300);
+
+
+    }
+
 }
