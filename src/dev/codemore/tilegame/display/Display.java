cfe99e6e2e5b5b79b4283fbfde77975832dcf971
Nina Ruiz <nina.ruizandres@gmail.com>
2020-08-31 12:29:26 +0200

    Create Display object and createDisplay method.

--- src/dev/codemore/tilegame/display/Display.java
+++ src/dev/codemore/tilegame/display/Display.java
@@ -1,4 +1,42 @@
 package dev.codemore.tilegame.display;
 
+import javax.swing.*;
+
 public class Display {
+
+    // Create Variables
+    private JFrame frame;
+    private String title;
+    private int width, height;
+
+    // Create Display object
+    public Display(String title, int width, int height){
+        // Instanced variables
+        this.title = title;
+        this.width = width;
+        this.height = height;
+
+        createDisplay();
+    }
+
+    // createDisplay method
+    private void createDisplay(){
+
+        // Create the window
+        frame = new JFrame(title);
+
+        // Set window size
+        frame.setSize(width,height);
+
+        // Close the program pressing x
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+
+        // Set the window at the center
+        frame.setLocationRelativeTo(null);
+
+        // Set the window visible
+        frame.setVisible(true);
+
+    }
+
 }
